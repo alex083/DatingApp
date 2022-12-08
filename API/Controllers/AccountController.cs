1773b152c552513b0798a42cb6ea1fa9e8887393
hordovenko.pro <hordovenko.pro@gmail.com>
2022-12-08 18:52:00 +0100

    Part 5 is finished

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -40,14 +40,14 @@ namespace API.Controllers
         }
 
         [HttpPost("login")]
-        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
                var user = await _context.Users.SingleOrDefaultAsync(x => 
                    x.UserName == loginDto.Username);
                 
-               if(user == null) return Unauthorized("invalid password");
+               if(user == null) {return Unauthorized("invalid username");}
 
-               using var hmac = new HMACSHA512(user.PasswordHash);
+               using var hmac = new HMACSHA512(user.PasswordSalt);
 
                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
@@ -55,7 +55,11 @@ namespace API.Controllers
                {
                     if(computedHash[i]!=user.PasswordHash[i]) return Unauthorized("invalid password");
                }
-               return user;
+            return new UserDto
+            {
+                Username = user.UserName,
+                Token = _tokenService.CreateToken(user)
+            };
         }
 
         private async Task<bool> UserExists(string username)
--- API/Program.cs
+++ API/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddCors();
 builder.Services.AddIdentityServices(builder.Configuration);
 
 // middleware
